d648e2031a681548f0e134e4e49393b272b53a44
suelitonoliveira <sueliton.oliveira@ewave.com.br>
2022-05-20 17:20:58 -0300

    aula 01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'may-firt-project';
+  title =  'my-firt-project'
+  nome :  string = 'faricio'
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './components/button/button.component';
-
+import { CouseListComponent } from './couses/curso-list.componentes';
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonComponent
+    ButtonComponent,
+    CouseListComponent,    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
+ 
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+//fabricio
+//seria igual uma casa,cada comodo tem suas partircularidades , limitador de contexto ex: produto, compra
+//componentes e o que vai da vida 
